04aa7704724772898249ecf7ee3a594247bfbbbf
bcirtlik <burcu-cirtlik@hotmail.com>
2019-10-18 14:56:09 +0300

    Resources

---
+++ src/GraphTheory/KruskalAlgorithm.java
@@ -0,0 +1,18 @@
+package GraphTheory;
+
+/*
+
+Finding Minimum Spanin Tree
+Difference Between Kruskal and Finding Min Path: In Kruskal you have to visit all the note with minimum cost
+DATA STRUCTURE: Array
+1- Sort All the Edges ascending order
+2- Process the edges one at a time
+3- Create array give each index its value
+4- If edge nodes does not belong the same group update index of the nodes with the same value
+5- If they belong the same group skip that part
+
+*/
+
+public class KruskalAlgorithm {
+
+}
