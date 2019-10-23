b4d4e6f135f14b76e4a7328828ad1aa8bf058426
bcirtlik <burcu-cirtlik@hotmail.com>
2019-10-23 10:32:20 +0300

    Kruskal

--- src/GraphTheory/KruskalAlgorithm.java
+++ src/GraphTheory/KruskalAlgorithm.java
@@ -1,18 +1,118 @@
 package GraphTheory;
 
-/*
+//https://www.hackerrank.com/challenges/kruskalmstrsub/problem
 
-Finding Minimum Spanin Tree
-Difference Between Kruskal and Finding Min Path: In Kruskal you have to visit all the note with minimum cost
-DATA STRUCTURE: Array
-1- Sort All the Edges ascending order
-2- Process the edges one at a time
-3- Create array give each index its value
-4- If edge nodes does not belong the same group update index of the nodes with the same value
-5- If they belong the same group skip that part
+import java.io.*;
+import java.util.*;
 
-*/
+class Result {
+
+
+    public static int kruskals(int gNodes, List<Integer> gFrom, List<Integer> gTo, List<Integer> gWeight) {
+        int totalWeight=0;
+        List<Node> nodes=new ArrayList<>();
+        for(int i=0;i<gWeight.size();i++){
+            nodes.add(new Node(gFrom.get(i),gTo.get(i),gWeight.get(i)));
+        }
+
+        Collections.sort(nodes);
+
+
+        int arr[]=new int[gNodes+1];
+
+        for(int i=1;i<=gNodes;i++){
+            arr[i]=i;
+        }
+
+
+        for(int i=0;i<nodes.size();i++){
+            if(allSameGroup(arr)){
+                break;
+            }
+            int to=nodes.get(i).nodeTo;
+            int from=nodes.get(i).nodeFrom;
+            int weight=nodes.get(i).weight;
+
+            if(arr[to]==arr[from])
+                continue;
+            else {
+                arr=merge(arr, to, from);
+                totalWeight += weight;
+            }
+        }
+
+        return totalWeight;
+
+    }
+    public static int[] merge(int arr[],int to,int from){
+        int temp=arr[to];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==temp)
+                arr[i]=arr[from];
+        }
+        return arr;
+    }
+
+
+    public static boolean allSameGroup(int arr[]){
+        int temp=arr[1];
+        for(int i=2;i<arr.length;i++){
+            if(arr[i]!=temp){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
 
 public class KruskalAlgorithm {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+
+        String[] gNodesEdges = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int gNodes = Integer.parseInt(gNodesEdges[0]);
+        int gEdges = Integer.parseInt(gNodesEdges[1]);
+
+        List<Integer> gFrom = new ArrayList<>();
+        List<Integer> gTo = new ArrayList<>();
+        List<Integer> gWeight = new ArrayList<>();
+
+        for (int i = 0; i < gEdges; i++) {
+            String[] gFromToWeight = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            gFrom.add(Integer.parseInt(gFromToWeight[0]));
+            gTo.add(Integer.parseInt(gFromToWeight[1]));
+            gWeight.add(Integer.parseInt(gFromToWeight[2]));
+        }
+
+        int res = Result.kruskals(gNodes, gFrom, gTo, gWeight);
+
+        System.out.println(res);
+
+        bufferedReader.close();
+    }
+}
+
+class Node implements Comparable<Node>{
+    public int nodeTo;
+    public int nodeFrom;
+    public int weight;
+
+    public Node(int nodeTo,int nodeFrom,int weight){
+        this.nodeTo=nodeTo;
+        this.nodeFrom=nodeFrom;
+        this.weight=weight;
+    }
+
+    public int compareTo(Node n){
+        return this.weight-n.getWeight();
+    }
+
+    public int getWeight(){
+        return this.weight;
+    }
 
 }
